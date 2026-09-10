947e2ed8c5c220b183714849928170234c9c93b9
JosueCH-17 <josue@datacurso.com>
2026-09-10 16:03:09 -0500

    fix: update plugin version number to 2026091100 in version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_dttutor';
-$plugin->version = 2026091000;
+$plugin->version = 2026091100;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.0.5';
