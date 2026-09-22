97c06c347445a00b890a3d1bb38439095623a90b
jose diaz <0196diaz@gmail.com>
2026-09-22 14:33:46 -0500

    chore(dttutor): bump version to 2.0.10

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_dttutor';
-$plugin->version = 2026091106;
+$plugin->version = 2026092200;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.9';
+$plugin->release = '2.0.10';
 // Plugin dependencies.
 $plugin->dependencies = [
     'aiprovider_datacurso' => 2026072300,
